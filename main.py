62d05017333d27971bb1665a431a68f8a49c2479
Freek Dijkstra <freek@macfreek.nl>
2024-10-20 16:23:43 +0200

    Rewrite of augment_cybenetics_reports()
    • Reports are now available on HTML page, so PDF parsing is no longer required.
    • Cybenetics reports contain inconsistencies. For example some Light Load Tests are done with 10W/20W/30W/40W instead of the usual 20W/40W/60W/80W. In 2 cases, the 
      - In 1 case, the "10W" test actually measured 20W. (report 1941
      - In 1 case, the "20W" test actually measured 10W. (report 2119)
      - In 1 case, the 230V test "#4" actually measured 100W instead of 80W (report 641)
    • Register Noise levels as well.
    - In 3 cases, no noise levels are registered, and those columns are missing. (reports 1872, 1926, 2485)
    • Always select 230V tests, if available, and 115V test if not.
      - in 15 cases, 230V is not available, and 115V test are used.
      - in 1 case, the input power was 220V instead of 230V. (report 2595)
      - in 1 case, the input power was 250V instead of 230V. (report 115)

--- main.py
+++ main.py
@@ -8,15 +8,37 @@ from urllib.parse import urlencode
 
 from alive_progress import alive_bar
 from bs4 import BeautifulSoup
-import fitz
 from pandas import DataFrame, read_csv, concat
 from pandas.errors import EmptyDataError
 import requests
 
-session = requests.Session()
+_downloader = None
 
 def randsleep():
     time.sleep(uniform(0.5, 2.5))
+
+def roundto(num, step=5, suffix="") -> str:
+    try:
+        if isinstance(num, str):
+            if num == '':
+                return ''
+            if not num[-1].isnumeric():
+                if not suffix:
+                    suffix = num[-1]
+                num = num[:-1]
+            num = float(num)
+        num = step*round(num/step)
+    except (ValueError, IndexError):
+        return None
+    return f'{num}{suffix}'
+
+
+def download_url(url: str) -> BeautifulSoup:
+    randsleep()
+    html = _downloader.get(url).text
+    soup = BeautifulSoup(html, "html5lib")
+    return soup
+
 
 def get_cybenetics_links() -> DataFrame:
     logger = logging.getLogger('efficient_psu')
@@ -24,6 +46,7 @@ def get_cybenetics_links() -> DataFrame:
         try:
             reports = read_csv("Reports.csv")
             if not reports.empty:
+                logger.info(f'Read {len(reports)} reports with {len(reports.columns)} columns from Reports.csv')
                 return reports
         except EmptyDataError as err:
             logger.warning(str(err))
@@ -32,8 +55,7 @@ def get_cybenetics_links() -> DataFrame:
     base_url = "https://www.cybenetics.com/"
     url = base_url + "index.php?option=database&params=2,1,0"
     logger.info(f"Loading {url}")
-    request = session.get(url)
-    soup = BeautifulSoup(request.text, "html5lib")
+    soup = download_url(url)
 
     table = soup.find(id="myTable")
     if not table:
@@ -65,9 +87,12 @@ def get_cybenetics_links() -> DataFrame:
     with alive_bar(len(brands)) as bar:
         for brand_id in brands:
             url = f'{base_url}code/db2.php?manfID={brand_id}&cert=0&bdg=&volts=2'
-            request = session.get(url)
             try:
-                soup = BeautifulSoup(request.text, "html5lib")
+                soup = download_url(url)
+            except Exception as err:
+                logger.error(f"Could not load {url}: {err}")
+                break
+            try:
                 # table = soup.find(id="myTable")
                 rows = soup.find_all("tr")
                 brandname = soup.find("th", class_="title").text
@@ -75,7 +100,7 @@ def get_cybenetics_links() -> DataFrame:
             except (AttributeError, IndexError) as err:
                 logger.warning(f"Could not parse table at {url}: {err}")
                 break
-            for index, row in enumerate(rows):
+            for row in rows:
                 td = row.find_all("td")
                 if len(td) < 11:
                     continue
@@ -110,72 +135,136 @@ def get_cybenetics_links() -> DataFrame:
                 logger.debug(repr(entry))
                 reports.append(entry)
             bar()
-            randsleep()
 
     reports = DataFrame.from_dict(reports)
     reports.to_csv("Reports.csv", encoding="utf-8", index=False)
-
+    logger.info(f'Write {len(reports)} reports with {len(reports.columns)} columns to Reports.csv')
     return reports
 
 
-def augment_cybenetics_reports(reports):
-    df = DataFrame(columns=["Brand", "Model", "Form Factor", "Cybenetics Rating", "20W Efficiency", "40W Efficiency", "60W Efficiency", "80W Efficiency", "Report Link", "Lowest Price (Geizhals.de)"])
-    print("Fetching individual PSU data...")
+def augment_cybenetics_reports(reports: DataFrame):
+    logger = logging.getLogger('efficient_psu')
+    if os.path.isfile("ReportsEfficiency.csv"):
+        try:
+            reports = read_csv("ReportsEfficiency.csv")
+            if not reports.empty:
+                logger.info(f'Read {len(reports)} reports with {len(reports.columns)} columns from ReportsEfficiency.csv')
+                return reports
+        except EmptyDataError as err:
+            logger.warning(str(err))
+            pass
+
+    logging.info("Fetching individual PSU data...")
+    # Add empty column to all records
+    EXPECTED_POWER = ('20W', '40W', '60W', '80W')
+    for power in EXPECTED_POWER:
+        reports[f"{power} Efficiency"] =  len(reports)*[None]
+    for power in EXPECTED_POWER:
+        reports[f"{power} Noise"] =  len(reports)*[None]
+    reports[f"Test Volt"] =  len(reports)*[None]
     with alive_bar(len(reports)) as bar:
-        for psu in reports:
-            response = requests.get(psu["Report Link"])
-            with open('/tmp/downloaded_pdf.pdf', 'wb') as pdf_file:
-                    pdf_file.write(response.content)
-            with open('/tmp/downloaded_pdf.pdf', 'rb') as file:
-                pdf_reader = fitz.open(file)
-                for page in pdf_reader:
-                    text = page.get_text()
-                    
-                    if not "20-80W LOAD TESTS" in text:
-                        continue
-                    title_count = -1
-                    for line in text.splitlines():
-                        title_count += 1
-                        if "20-80W LOAD TESTS" in line:
-                            break
+        for idx, psu in tqdm(reports.iterrows()):
+            url = psu["Report Link"]
+            if not str(url).startswith('http'):
+                continue
+            try:
+                soup = download_url(url)
+            except Exception as err:
+                logger.error(f"Could not load {url}: {err}")
+                return
+            try:
+                # table = soup.find(id="myTable")
+                section = soup.find(id='supllem-1')
+                header = section.find('h3').text
+                if not header or not header.startswith('Light Load Tests'):
+                    logging.error(f"Expected Light Load Test section in {url}. Found {header!r}")
+                    continue
+                tables = section.find_all('table')
+                # print(url, len(tables), header)
+                if not tables:
+                    logging.error(f"Could not find Light Load Test table in {url}")
+                    continue
+                table = tables[-1]
+                # table = section.find(id='nav-YimmShBtm8-20-80-230')
+                rows = table.find_all("tr")
+                wall_power = table.find_all("td")[-1].text
+                reports.at[idx,f"Test Volt"] = roundto(wall_power, suffix="V")
+            except (AttributeError, IndexError) as err:
+                logger.warning(f"Could not parse table at {url}: {err}")
+                return
 
-                    efficiency = [re.search(r"^.*%", line).group(0) for line in text.splitlines()[title_count:] if re.search(r"^.*%", line) is not None]
+            if len(rows) <= 2:
+                logger.warning(f"Can not parse table Light Load Tests in {url}: expected 9 rows, Found {len(rows)}")
+                continue
+            td = rows[0].find_all("td")
+            if len(td) < 9:
+                logger.warning(f"Can not parse table Light Load Tests in {url}: expected 11 columns, Found {len(td)}")
+                continue
+            if not td[0].text.startswith('Test'):
+                logger.warning(f"Expected Column 'Test' in Light Load Tests on {url}. Got {td[0].text!r}")
+                continue
+            if td[6].text != 'Efficiency':
+                logger.warning(f"Expected Column 'Efficiency' in Light Load Tests on {url}. Got {td[6].text!r}")
+                continue
+            if 'Watts' not in td[5].text:
+                logger.warning(f"Expected Column 'DC/AC (Watts)' in Light Load Tests on {url}. Got {td[5].text!r}")
+                continue
+            if len(td) > 7 and 'Noise' in td[8].text:
+                noise_col = 8
+            else:
+                logger.warning(f"Expected Column 'PSU Noise' in Light Load Tests on {url}. Got {td[8].text!r}")
+                noise_col = None
 
-                    df_new = DataFrame([{"Brand": psu['Brand'], 
-                                            "Model": psu['Model'], 
-                                            "Form Factor": psu['Form Factor'], 
-                                            "Cybenetics Rating": psu['Cybenetics Rating'], 
-                                            "20W Efficiency": efficiency[0],
-                                            "40W Efficiency": efficiency[1],
-                                            "60W Efficiency": efficiency[2],
-                                            "80W Efficiency": efficiency[3],
-                                            "Report Link": psu['Report Link'],
-                                            "Lowest Price (Geizhals.de)": psu['Lowest Price (Geizhals.de)']
-                                            }])
-                    df = concat([df, df_new], ignore_index=True)
 
-            open('/tmp/downloaded_pdf.pdf', 'w').close()
-            bar()
-    return df
+            for row in rows[1:]:
+                td = row.find_all("td")
+                if not td[0].attrs.get('rowspan'):
+                    continue
+                if len(td) < 8:
+                    logger.warning(f"Can not parse table Light Load Tests in {url}: expected 11 columns, Found {len(td)}")
+                    continue
+                power = roundto(td[5].text, suffix='W')
+                if power is None:
+                    logger.error(f"Expected Power in Watts in Light Load Tests on {url}. Got {td[5].text!r}")
+                    continue
+                testname = td[0].text
+                if testname not in (power, '1', '2', '3', '4', '5', '6', '7', '8'):
+                    logger.warning(f"Light Load Tests {testname!r} on {url} measured {td[5].text} Watt.")
+                    # continue
+                if power not in EXPECTED_POWER:
+                    logger.warning(f"Skip Light Load Tests {testname!r} on {url} with {power} Watt.")
+                    continue
+                efficiency = td[6].text
+                if noise_col is None:
+                    noise = None
+                else:
+                    noise = td[8].text
+                reports.at[idx,f"{power} Efficiency"] = efficiency
+                reports.at[idx,f"{power} Noise"] = noise
+            # if idx > 20:
+            #     break
+        bar()
+
+        reports.to_csv("ReportsEfficiency.csv", encoding="utf-8", index=False)
+        logger.info(f'Write {len(reports)} reports with {len(reports.columns)} columns to ReportsEfficiency.csv')
 
 
-def augment_geizhals_prices(reports):
+def augment_geizhals_prices(reports: DataFrame):
     if os.path.isfile("ReportsPriced.csv"):
         reports = read_csv("ReportsPriced.csv")
         reports = reports.to_dict('records')
         if reports:
             return reports
 
-    print("Getting prices...")
-    request = session.get("https://geizhals.de/?cat=gehps")
+    logging.info("Getting prices...")
+    url = "https://geizhals.de/?cat=gehps"
+    soup = download_url(url)
     # Click on 'Accept cookies'
     try:
         request.text.find_element("id", "onetrust-accept-btn-handler")
-        randsleep()
     except NoSuchElementException:
         pass
 
-    randsleep()
     for psu in reports:
         if re.match(r".*(Sample|#\d+).*", psu["Model"]):
             price = None
@@ -188,14 +277,11 @@ def augment_geizhals_prices(reports):
                     'bl1_id': 30
                     }
             url = f"https://geizhals.de/?{urlencode(params)}"
-            print(url)
-            request = session.get(url)
-            randsleep()
+            soup = download_url(url)
             try:
                 no_results = driver.find_element(By.CLASS_NAME, "category_list__empty-list")
                 price = None
             except NoSuchElementException:
-                randsleep()
                 soup = BeautifulSoup(request.text, "html5lib")
                 price = soup.find("div", {"id": "product0"})\
                             .find("div", {"class": "cell productlist__price"})\
@@ -211,16 +297,18 @@ def augment_geizhals_prices(reports):
     return reports
 
 
-def write_reports(data):
-    print(data)
-    data.to_csv("PSUs.csv", encoding='utf-8', index=False)
+def write_reports(reports: DataFrame):
+    logger = logging.getLogger('efficient_psu')
+    reports.to_csv("PSUs.csv", encoding='utf-8', index=False)
+    logger.info(f'Write {len(reports)} reports with {len(reports.columns)} columns to PSUs.csv')
+    print(reports)
 
 
 def main():
     reports = get_cybenetics_links()
     if reports.empty:
         return
-    # augment_cybenetics_reports(reports)
+    augment_cybenetics_reports(reports)
     # reports = augment_geizhals_prices(reports)
     # augment_amazon_prices(reports)
     # augment_tweakers_prices(reports)
@@ -229,4 +317,5 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    _downloader = requests.Session()
     main()
